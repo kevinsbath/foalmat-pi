51d4e99ec3cc31fdb0314f86519772d90117d0e6
kevinsbath <kevin@flowmo.co>
2019-01-13 17:45:22 +0000

    messing with fonts

--- foalmat.py
+++ foalmat.py
@@ -1,7 +1,17 @@
 import scrollphathd as sch
 import time
+from scrollphathd.fonts import font5x5
 
-sch.set_pixel(1, 1, 1)
+sch.clear()
+sch.rotate(180)
+
+sch.write_string('>>> COME BACK IN..... ')
+
+for i in range(100):
+	sch.scroll(1)
+	sch.show()
+
+sch.rotate(180)
 sch.show()
 
-time.sleep(3)
+time.sleep(3000)
---
+++ foalmat.py~
@@ -0,0 +1,7 @@
+import scrollphathd as sch
+import time
+
+sch.set_pixel(1, 1, 1)
+sch.show()
+
+time.sleep(3)
