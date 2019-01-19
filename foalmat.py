bfb4d198c6b459c2dccae3b96e88be1c0ae7022f
kevinsbath <kevin@flowmo.co>
2019-01-19 16:46:59 +0000

    tidied up code a tad

--- foalmat.py
+++ foalmat.py
@@ -1,46 +1,41 @@
+
 import scrollphathd as sch
 import time
 from scrollphathd.fonts import font5x5
+
+def sch_setup():
+	sch.clear()
+	sch.rotate(180)
+
+
+
+def sch_setup_string(display_string):
+	sch.write_string(
+		display_string,
+		x=50,
+		y=0,
+		brightness=0.3,
+	)
+
+	for i in range(150):
+		sch.scroll(1)
+		sch.show()
+		time.sleep(0.02)
+
 
 def get_time_left():
 	secs = time.time() - start_time
 	mins = secs/60
 	return mins
 
-mins_to_wait = 99
-start_time = time.time()
-
-sch.clear()
-sch.rotate(180)
-
-sch.write_string(
-	'>>> COME BACK IN..... ',
-	x=50,
-	y=0,
-	brightness=0.3,
-)
-
-for i in range(150):
-	sch.scroll(1)
-	sch.show()
-	time.sleep(0.02)
-
-sch.clear()
+def main():
+	mins_to_wait = 99
+	start_time = time.time()
+	sch_setup()
+	sch_display_string(">>> Come back in ....")
 
 
 
-
-sch.write_string(
-	"88m",
-	x=0,
-	y=0,
-	brightness=0.1,
-)
-sch.show()
-
-print get_time_left()
-
-sch.rotate(180)
-sch.show()
-
-time.sleep(3000)
+if __name__ == "__main__":
+	main()
+	time.sleep(3000)
